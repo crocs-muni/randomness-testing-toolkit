dfbf5999f9ee9ffec5e3c953842465308eb9afc1
xobrati1 <lubomir.obratil@gmail.com>
2017-04-05 17:14:56 +0200

    Added script for creating tables into storage directory

---
+++ rtt/storage/create_rtt_tables.sql
@@ -0,0 +1,124 @@
+DROP DATABASE IF EXISTS rtt;
+
+CREATE DATABASE IF NOT EXISTS rtt;
+USE rtt;
+
+CREATE TABLE IF NOT EXISTS experiments (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(255) NOT NULL,
+    author_email        VARCHAR(255),
+    created             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status              ENUM('pending','running','finished') NOT NULL DEFAULT 'pending',
+    config_file         VARCHAR(255) NOT NULL,
+    data_file           VARCHAR(255) NOT NULL,
+    data_file_sha256    VARCHAR(64) NOT NULL
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS jobs (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    battery             VARCHAR(100) NOT NULL,
+    status              ENUM('pending','running','finished') NOT NULL DEFAULT 'pending',
+    experiment_id       BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (experiment_id) REFERENCES experiments(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS batteries (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(255) NOT NULL,
+    passed_tests        BIGINT UNSIGNED NOT NULL,
+    total_tests         BIGINT UNSIGNED NOT NULL,
+    alpha               DOUBLE NOT NULL,
+    experiment_id       BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (experiment_id) REFERENCES experiments(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS battery_errors (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message             VARCHAR(1000) NOT NULL,
+    battery_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (battery_id) REFERENCES batteries(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS battery_warnings (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message             VARCHAR(1000) NOT NULL,
+    battery_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (battery_id) REFERENCES batteries(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS tests (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(255) NOT NULL,
+    partial_alpha       DOUBLE NOT NULL,
+    result              ENUM('passed', 'failed') NOT NULL,
+    test_index          INT UNSIGNED NOT NULL,
+    battery_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (battery_id) REFERENCES batteries(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS variants (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    variant_index       INT UNSIGNED NOT NULL,
+    test_id             BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (test_id) REFERENCES tests(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS variant_warnings (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message             VARCHAR(1000) NOT NULL,
+    variant_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (variant_id) REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS variant_errors (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message             VARCHAR(1000) NOT NULL,
+    variant_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (variant_id) REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS variant_stderr (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    message             VARCHAR(1000) NOT NULL,
+    variant_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (variant_id) REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS user_settings (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(100) NOT NULL,
+    value               VARCHAR(50) NOT NULL,
+    variant_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (variant_id) REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS subtests (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    subtest_index       INT UNSIGNED NOT NULL,
+    variant_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (variant_id) REFERENCES variants(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS statistics (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(255) NOT NULL,
+    value               DOUBLE NOT NULL,
+    result              ENUM('passed', 'failed') NOT NULL,
+    subtest_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (subtest_id) REFERENCES subtests(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS test_parameters (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name                VARCHAR(100) NOT NULL,
+    value               VARCHAR(50) NOT NULL,
+    subtest_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (subtest_id) REFERENCES subtests(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
+
+CREATE TABLE IF NOT EXISTS p_values (
+    id                  BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    value               DOUBLE NOT NULL,
+    subtest_id          BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (subtest_id) REFERENCES subtests(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = INNODB;
